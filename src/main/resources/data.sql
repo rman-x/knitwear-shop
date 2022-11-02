7dd7fbb4b2dbaa61ec7893e3810e9eb9549a8b63
rman-x <r4manpetrov@gmail.com>
2022-11-03 00:42:30 +0600

    feat: change initial sql script for database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO items (title, price, image_url) VALUES 
-('плюшевый мишка', 100, 'img/items/1.svg'),
-('раз мишка', 200, 'img/items/2.svg'),
-('плюшевый раз', 140, 'img/items/3.svg'),
-('раз два', 150, 'img/items/4.svg');
-
-INSERT INTO cart (parent_id) VALUES 
-(1), (2);
+('Меховая шапка', 100, 'img/items/1.jpg'),
+('Тканая сумочка', 150, 'img/items/2.jpg'),
+('Клубок нитей', 20, 'img/items/3.jpg'),
+('Толстые нити', 10, 'img/items/4.jpg'),
+('Вязанные полотенца', 60, 'img/items/5.jpg'),
+('Тканые шарфы', 80, 'img/items/6.jpg'),
+('Я хз что это', 100, 'img/items/7.jpg');
