686364bcb896fe41c5f04de6a949272dd4ae6d04
rman-x <r4manpetrov@gmail.com>
2022-11-03 00:32:24 +0600

    fix: disable cors

--- src/main/java/edu/omstu/shop/controller/CartController.java
+++ src/main/java/edu/omstu/shop/controller/CartController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +33,7 @@ public class CartController {
     }
     
     @DeleteMapping("/{id}")
-    public void delete(@RequestParam Long id) {
+    public void delete(@PathVariable("id") Long id) {
         service.remove(id);
     }
 
